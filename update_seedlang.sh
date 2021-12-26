fbb7fee9eb7429ecc7e056ddfd793ecab749e9de
wixette <wixette@gmail.com>
2021-12-26 12:07:07 +0800

    Update copyright lines

--- update_seedlang.sh
+++ update_seedlang.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2021 The Aha001 Team.
+# Copyright 2021-2022 The SeedV Lab.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
